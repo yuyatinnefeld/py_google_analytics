56ce7aaf1c19090a76bbe2d2cdcec882aa09ee2f
yuyatinnefeld <yuyatinnefeld@gmail.com>
2021-02-01 13:13:41 +0100

    extented ga

--- project/main.py
+++ project/main.py
@@ -1,14 +1,16 @@
 from decouple import config
 from oauth import setup_analytics
-from google_analytics import get_response, ga_response_dataframe
-
+from google_analytics import (
+    get_response, ga_response_dataframe, set_daterange, set_metrics, set_dimensions)
 
 VIEW_ID = config('VIEW_ID')
+data_range = set_daterange('2020-01-01', '2020-02-01')
+metrics = set_metrics('ga:pageviews','ga:avgSessionDuration')
+dimensions = set_dimensions('ga:deviceCategory')
 
 analytics = setup_analytics()
-response = get_response(analytics, VIEW_ID)
-result = ga_response_dataframe(response)
+response1 = get_response(analytics, VIEW_ID, data_range, metrics, dimensions)
+df = ga_response_dataframe(response1)
 
-
-print(result)
+print(df.head())
 
